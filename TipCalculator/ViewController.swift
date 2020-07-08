e979d67d83f3ce2cf54787a36fb040813d2dacc4
Peter Shelley <petershe@live.unc.edu>
2020-07-08 10:41:00 -0400

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Peter Shelley on 7/8/20.
+//  Copyright © 2020 Peter Shelley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
